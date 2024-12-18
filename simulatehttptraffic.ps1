333662a6aa6490007dc197f1c783ba81dea900f7
Mario Cuomo <39244192+mariocuomo@users.noreply.github.com>
2024-12-18 17:06:12 +0100

    updt

---
+++ simulatehttptraffic.ps1
@@ -0,0 +1,52 @@
+$context = (Connect-AzAccount -Identity).context
+$token = Get-AzAccessToken -ResourceUrl "https://management.azure.com/" -TenantId $context.Tenant.Id
+$authHeader = @{
+    'Content-Type'  = 'application/json'
+    'Authorization' = 'Bearer ' + $token.Token
+}
+$subscriptionId = $context.Subscription.Id
+
+$resourceGroupName = "ITOperations"
+$appServiceName = "ASP-CyberSOC-b341"
+$apiVersion = "2024-04-01"
+
+$url = "https://management.azure.com/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.Web/serverfarms/$appServiceName/sites?api-version=$apiVersion"
+
+$token = (Get-AzAccessToken).Token
+
+$response = Invoke-RestMethod -Uri $url -Method Get -Headers @{ Authorization = "Bearer $token" }
+
+$baseUrl = "https://"+$response.value[0].properties.defaultHostName
+$paths = @(
+    "/index.php",
+    "/",
+    "/index.php?page=home",
+    "/index.php?page=about",
+    "/index.php?page=services",
+    "/index.php?page=contact",
+    "/?page=home",
+    "/?page=about",
+    "/?page=services",
+    "/?page=contact",
+    "/index.php?page=hom",
+    "/index.php?page=abouts",
+    "/index.php?page=service",
+    "/index.php?page=contacts",
+    "/?page=hom",
+    "/?page=abouts",
+    "/?page=service",
+    "/?page=contacts"
+)
+
+function Get-RandomUrl {
+    $randomPath = Get-Random -InputObject $paths
+    return "$baseUrl$randomPath"
+}
+
+$randomCount = Get-Random -Minimum 1 -Maximum 101
+
+for ($i = 0; $i -lt $randomCount; $i++) {
+    $url = Get-RandomUrl
+    Invoke-WebRequest -Uri $url -Method GET
+    Start-Sleep -Seconds 1
+}
