4c5f3bc6d1ae3e9e8b5f31fd1b9b468d216b19b7
Thomas Bründl <58534974+Warfion@users.noreply.github.com>
2024-12-19 09:52:44 +0100

    Update Set-MsSentintelTableRetention-Runbook_AA.ps1

--- Set-MsSentintelTableRetention-Runbook_AA.ps1
+++ Set-MsSentintelTableRetention-Runbook_AA.ps1
@@ -1,65 +1,52 @@
 #SETTINGS
-$WorkspaceName = "CyberSOCWS"
+$ResourceGroup = "CyberSOC"
+$Workspace = "CyberSOCWS"
 $TableNames = @("AzureActivity")
 $RetentionInDays = 90
 $TotalRetentionInDays = 120
 
-try {
-    Connect-AzAccount -Identity
+$context = (Connect-AzAccount -Identity).context
+$token = Get-AzAccessToken -ResourceUrl "https://management.azure.com/" -TenantId $context.Tenant.Id
+$authHeader = @{
+    'Content-Type'  = 'application/json'
+    'Authorization' = 'Bearer ' + $token.Token
 }
-catch {
-    Write-Error "Failed to get or connect to Azure context: $_" -ErrorAction Stop
-}
+$SubscriptionId = $context.Subscription.Id
 
-$workspace = Get-AzResource -Name $WorkspaceName -ResourceType 'Microsoft.OperationalInsights/workspaces'
+$argHash = @{}
+$argHash.properties = @{
+    retentionInDays         = "$RetentionInDays"
+    totalRetentionInDays  = "$TotalRetentionInDays"
+}
 
 $tables = [System.Collections.Generic.List[PSObject]]::new()
 
 foreach ($TableName in $TableNames) {
-    if ($null -ne $workspace) {
-        $apiVersion = '?api-version=2023-09-01'
-        $baseUri = '{0}/Tables/{1}' -f $workspace.ResourceId,$TableName
-        $tablelistpath = '{0}/{1}' -f $baseUri,$apiVersion
-        }
-    else {
-        Write-Error "Unable to to retrieve log analytics workspace with error code: $($_.Exception.Message)" -ErrorAction Stop
-    }
-
-    $argHash = @{}
-    $argHash.properties = @{
-        retentionInDays         = "$RetentionInDays"
-        totalRetentionInDays  = "$TotalRetentionInDays"
-    }
+    $serverUrl = "https://management.azure.com"
+    $baseUri = $serverUrl + "/subscriptions/${SubscriptionId}/resourceGroups/${ResourceGroup}/providers/Microsoft.OperationalInsights/workspaces/${Workspace}/Tables/${TableName}/?api-version=2023-09-01"
 
     try {
-        $result = Invoke-AzRestMethod -Path $tablelistpath -Method PUT -Payload ($argHash | ConvertTo-Json)
-        if ($result.StatusCode -ne 200) {
-            Write-Error ($result | ConvertFrom-Json)
+        Invoke-RestMethod -Uri $baseUri -Method "Put" -Headers $AuthHeader -Body ($argHash  | ConvertTo-Json -EnumsAsStrings -Depth 50)
         }
-    }
     catch {
         Write-Error "Unable to update the table with error code: $($_.Exception.Message)" -ErrorAction Stop
     }
 
     try {
-        $webData = Invoke-AzRestMethod -Path $tablelistpath -Method GET 
-        if ($webData.StatusCode -eq 200) {
-            $webData = ($webData.Content | ConvertFrom-Json)
+        $webData = Invoke-RestMethod -Method "Get" -Uri $baseUri -Headers $authHeader
                 $table = [PSCustomObject]@{
-                    TableName = $webdata.name
+                    WorkspaceName = $webdata.name
                     RetentionInDays = $webdata.properties.retentionInDays
-                    ArchiveRetentionInDays = $webdata.properties.archiveRetentionInDays
-                    TotalRetentionInDays = $webdata.properties.totalRetentionInDays
+                    ArchiveRetentionInDays = $webData.properties.archiveRetentionInDays
+                    TotalRetentionInDays = $webData.properties.totalRetentionInDays
             }
             $tables.Add($table)
-        }
-        else {
-            Write-Error ($webData.Content | ConvertFrom-Json)
-        }
     }
     catch {
         Write-Error "Unable to list the table with error code: $($_.Exception.Message)" -ErrorAction Stop
     }
+
+
 }
 
 return $tables
