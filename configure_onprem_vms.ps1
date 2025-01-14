f74d9b602408e8254028da33a7616d3ff5390c0f
Nikolay Salnikov <nsalnikov@gmail.com>
2025-01-14 23:42:44 +0400

    Update configure_onprem_vms.ps1

--- configure_onprem_vms.ps1
+++ configure_onprem_vms.ps1
@@ -1343,6 +1343,7 @@ Write-Output "`n[+] There should be Event Log for a Type 2 logon for `$UserName.
 
 Write-Output "[+] Emulating RS running under candice..."
 schtasks /create /tn "RunReverseShell" /tr "'C:\Users\candice.kevin\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\rs.exe'" /sc ONCE /st 23:59 /ru "ODOMAIN\candice.kevin" /rp "`$Password"  /RL HIGHEST  /F 
+start-sleep 5
 schtasks /run /tn "RunReverseShell"
 
 Write-Output "`n[+] There should be 4688 events for rs.exe."
